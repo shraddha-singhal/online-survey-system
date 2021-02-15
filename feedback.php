d5e5875d7d8d14030828a371a971521a8636bd79
shraddha-singhal <78748569+shraddha-singhal@users.noreply.github.com>
2021-02-15 18:23:58 +0530

    Update feedback.php

--- feedback.php
+++ feedback.php
@@ -18,6 +18,7 @@
 	<section id="im" style="margin-top: -20px;">
 		<br><br><br><br>
 		<div class="box4">
+			<form action="" method="POST">
 			<h1
 			<p style="font-size: 20px;">Do you like the website?</p><br>
 			<input type="radio" name="b" value="yes" id="abc">YES&nbsp&nbsp&nbsp
@@ -34,10 +35,10 @@
 			<input type="radio" name="a" value="poor">Poor<br>
 			<br><br>
 			<p style="font-size: 20px;">Would you like to visit our website again?</p><br>
-			<input type="radio" name="yes" value="yes">YES&nbsp&nbsp&nbsp
-			<input type="radio" name="yes" value="no">NO
+			<input type="radio" name="y" value="yes">YES&nbsp&nbsp&nbsp
+			<input type="radio" name="y" value="no">NO
 			<br><br>
-			<form action="" method="POST">
+			
 				<h3 style="font-size: 25px;">Any Suggestion</h3><br><br>
 				<input type="text" name="suggestion" style="width:250px;height:50px; color: black;">&nbsp&nbsp
 				<input class="btn btn-default" type="submit" name="submit" value="Submit" style="color: black; width: 70px; height: 30px">
@@ -51,8 +52,12 @@
 
 	        if(isset($_POST['submit']))
 	        {
+			$b=($_POST['b']);
+			$p=($_POST['p']);
+			$a=($_POST['a']);
+			$y=($_POST['y']);
 	        	$suggestion=($_POST['suggestion']);
-	        	mysqli_query($db,"INSERT INTO feedback(suggestion)VALUES('$suggestion')");
+	        	mysqli_query($db,"INSERT INTO feedback(c1,c2,c3,c4,suggestion)VALUES('$b','$p','$a','$y','$suggestion')");
 	        	header("location:feedback.php");
 
 
